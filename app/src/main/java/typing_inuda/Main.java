a20b984cd589d3e4da875ff47156abca9c1b591d
Nagai <annabel3077@gmail.com>
2023-08-30 12:04:35 +0900

    メインクラス
    新規追加

---
+++ app/src/main/java/typing_inuda/Main.java
@@ -0,0 +1,10 @@
+import front.window.MainWindow;
+import front.screen_mode.ScreenMode;
+
+public class Main {
+    public static void main (String args[]) {
+        MainWindow.getInstance(ScreenMode.TITLE);
+    }
+}
+
+
