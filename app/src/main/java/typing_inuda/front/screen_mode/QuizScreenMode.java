163d9197d222690bbff2200c41362d009f44c43b
Nagai <annabel3077@gmail.com>
2023-08-30 11:47:50 +0900

    enum追加
    新規追加

---
+++ app/src/main/java/typing_inuda/front/screen_mode/QuizScreenMode.java
@@ -0,0 +1,7 @@
+package typing_inuda.front.screen_mode;
+
+public enum QuizScreenMode {
+    QUIZ_TITLE,
+    QUIZ_RESULT,
+    QUIZ_GAME,
+}
---
+++ app/src/main/java/typing_inuda/front/screen_mode/ScreenMode.java
@@ -0,0 +1,7 @@
+package typing_inuda.front.screen_mode;
+
+public enum ScreenMode {
+    QUIZ,
+    ADMIN,
+    TITLE,
+}
